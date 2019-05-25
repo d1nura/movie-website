326cba38778653576a4ea8d39c7e2960bff262ad
d1nura <ddakshitha1@outlook.com>
2019-05-25 13:27:37 +0530

    after menu

--- src/components/FirstPage.js
+++ src/components/FirstPage.js
@@ -4,25 +4,50 @@ import { PageContext } from "./PageContext";
 import HeroContent from "../components/HeroContent";
 import MovieSet from "./MovieSet";
 import "../scss/FirstPage.scss";
+import menu from "../assets/images/circled.svg";
 //import down from "../assets/images/download.svg";
 
 function FirstPage() {
   let [optionVal, setOptionVal] = useState("movie/popular?");
   let [data, load] = useHttp(optionVal);
+  let [menuOn, setMenuOn] = useState(false);
 
   const setSelectVal = e => {
     setOptionVal(e.target.value);
     console.log(optionVal);
   };
 
+  const showMenu = () => {
+    console.log(1);
+    menuOn === true ? setMenuOn(false) : setMenuOn(true);
+  };
+
   const setFirstPage = () => {
     return (
       <React.Fragment>
-        <select id="popularity" onChange={setSelectVal}>
-          <option value="movie/top_rated?">Top Rated</option>
-          <option value="movie/popular?">Popular</option>
-          <option value="movie/now_playing?">Now Playing</option>
-        </select>
+        <div id="menuIcon">
+          <img
+            alt="menuIcon"
+            onClick={showMenu}
+            src={menu}
+            style={{ width: "2em" }}
+          />
+        </div>
+        <div
+          id="basicMenu"
+          className={menuOn === true ? "SelectMenu" : "HideMenu"}
+        >
+          <select id="popularity" onChange={setSelectVal}>
+            <option value="movie/top_rated?">Top Rated</option>
+            <option defaultValue value="movie/popular?">
+              Popular
+            </option>
+            <option value="movie/now_playing?">Now Playing</option>
+          </select>
+          <div id="genreList">
+            <p>Genres</p>
+          </div>
+        </div>
         <PageContext.Provider value={data}>
           <HeroContent />
           <MovieSet />
